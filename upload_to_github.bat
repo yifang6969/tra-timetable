268c1b924c4eac98b7e2b8658ae12e646d8fa617
YiFang Chang <? GitHub ?>
2025-07-16 09:31:03 +0800

    Deploy to Render

--- upload_to_github.bat
+++ upload_to_github.bat
@@ -1,19 +1,23 @@
 @echo off
 cd /d "%~dp0"
 
-:: 設定 GitHub repo 名稱（只需設定一次）
 set REPO_NAME=tra-timetable
 set GITHUB_USERNAME=yifang6969
 
-:: 初始化 git（第一次才需要）
-IF NOT EXIST ".git" (
+:: 初始化 Git（如果還沒 init）
+if not exist ".git" (
     git init
     git remote add origin https://github.com/%GITHUB_USERNAME%/%REPO_NAME%.git
 )
 
-:: 加入所有檔案、提交並 push
+:: 設定使用者資訊（第一次使用）
+git config user.name "YiFang Chang"
+git config user.email "你的 GitHub 註冊信箱"
+
+:: 提交 & 上傳
 git add .
 git commit -m "Deploy to Render"
+git branch -M main
 git push -u origin main
 
 pause
